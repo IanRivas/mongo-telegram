78780c084b8ae21c2e82fdcd2a72a212f89c7879
giornoRivas <leondantex@gmail.com>
2021-07-13 01:19:54 -0300

    manejador de rutas funcionando

--- components/message/network.js
+++ components/message/network.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 const response = require("../../network/response.js");
 
-router.get('/message',function(req, res){
+router.get('/',function(req, res){
     console.log(req.query.saludo);
     console.log(req.headers);
     response.success(req, res,"lista de mensajes",333);
 })
 
-router.post('/message',function(req, res){
+router.post('/',function(req, res){
     if(req.query.error == 'okey'){
         response.error(req,res,"error simulado", 400, "error para no mostrar al cliente");
     } else {
@@ -18,3 +18,6 @@ router.post('/message',function(req, res){
 });
 
 module.exports = router;
+
+// el router de mi componente lo recibe routes.js y ahi routes recibe la app y 
+// le aplica todas las rutas de todos los componentes 
--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require('express');
 require("dotenv").config();
-const router = require('./components/message/network');
+const router = require('./network/routes.js');
 
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-app.use(router);
+
+// app.use(router);
+router(app);
+//la funcion que tengo en el routes que aca se llama router le tengo que pasar un servidor
+//le paso la app ,y a este le aplica use y le pasa una ruta y el router del component
 
 
 app.use('/app',express.static('./public'));
